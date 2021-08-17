760fa249ea922ba2dd9926e830b02eef3724d71d
Ross Johnstone <rhjohnstone@gmail.com>
2021-08-17 12:42:44 +0900

    Detect faces in still image

---
+++ detect-faces.py
@@ -0,0 +1,29 @@
+import cv2 as cv
+import numpy as np
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+from time import sleep
+
+
+def main():
+    face_cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
+    
+    with PiCamera(resolution=(816, 480), framerate=12) as camera:
+        camera.rotation = 180
+        camera.start_preview()
+        sleep(3)
+        with PiRGBArray(camera) as output:
+            camera.capture(output, "bgr")
+            image = output.array
+        #camera.stop_preview()
+    grey = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(grey, 1.1, 5)
+    for x, y, w, h in faces:
+        cv.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 2)
+    cv.imshow("faces?", image)
+    cv.waitKey()
+    cv.destroyAllWindows()
+    
+
+if __name__ == "__main__":
+    main()
